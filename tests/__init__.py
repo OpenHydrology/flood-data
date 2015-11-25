9a992eacce6657d1af82abb5568ad36e98250d13
Florenz A. P. Hollebrandse <faph@users.noreply.github.com>
2015-11-25 19:03:50 +0000

    yield test for each catchment

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 
-import unittest
+import nose.tools as nt
 import os
 from floodestimation import parsers
 from floodestimation import entities
 
 
-class TestValidFiles(unittest.TestCase):
+class TestValidFiles(object):
     def _files_by_ext(self, ext):
         return [os.path.join(dp, f) for dp, dn, filenames in os.walk('./data')
                 for f in filenames if os.path.splitext(f)[1].lower() == ext]
@@ -18,42 +18,51 @@ class TestValidFiles(unittest.TestCase):
 
     def test_cd3_files(self):
         for cd3_fp in self._files_by_ext('.cd3'):
-            cd3_fn = os.path.basename(cd3_fp)
-            c = parsers.Cd3Parser().parse(cd3_fp)
-            self.assertTrue(isinstance(c.descriptors.centroid_ngr, entities.Point),
-                            msg="Catchment {} does not have centroid coordinates.".format(cd3_fn))
-            self.assertGreater(c.descriptors.dtm_area, 0,
-                               msg="Catchment {} does not have a `dtm_area`>0.".format(cd3_fn))
-            self.assertLess(c.descriptors.dtm_area, 10000,
-                            msg="Catchment {} does not have a `dtm_area`<10000.".format(cd3_fn))
-            self.assertGreaterEqual(c.descriptors.bfihost, 0,
-                                    msg="Catchment {} does not a `bfi_host`>=0.".format(cd3_fn))
-            self.assertLessEqual(c.descriptors.bfihost, 1,
-                                 msg="Catchment {} does not a `bfi_host`<=1.".format(cd3_fn))
-            self.assertGreater(c.descriptors.saar, 250,
-                               msg="Catchment {} does not have a `saar`>250.".format(cd3_fn))
-            self.assertLess(c.descriptors.saar, 5000,
-                            msg="Catchment {} does not have a `saar`<5000.".format(cd3_fn))
+            yield self.check_cd3_file, cd3_fp
+
+    def check_cd3_file(self, cd3_fp):
+        cd3_fn = os.path.basename(cd3_fp)
+        c = parsers.Cd3Parser().parse(cd3_fp)
+        nt.assert_true(isinstance(c.descriptors.centroid_ngr, entities.Point),
+                       msg="Catchment {} does not have centroid coordinates.".format(cd3_fn))
+        nt.assert_greater(c.descriptors.dtm_area, 0,
+                          msg="Catchment {} does not have a `dtm_area`>0.".format(cd3_fn))
+        nt.assert_less(c.descriptors.dtm_area, 10000,
+                       msg="Catchment {} does not have a `dtm_area`<10000.".format(cd3_fn))
+        nt.assert_greater_equal(c.descriptors.bfihost, 0,
+                                msg="Catchment {} does not a `bfi_host`>=0.".format(cd3_fn))
+        nt.assert_less_equal(c.descriptors.bfihost, 1,
+                             msg="Catchment {} does not a `bfi_host`<=1.".format(cd3_fn))
+        nt.assert_greater(c.descriptors.saar, 250,
+                          msg="Catchment {} does not have a `saar`>250.".format(cd3_fn))
+        nt.assert_less(c.descriptors.saar, 5000,
+                       msg="Catchment {} does not have a `saar`<5000.".format(cd3_fn))
 
     def test_am_files(self):
         for am_fp in self._files_by_ext('.am'):
-            am_fn = os.path.basename(am_fp)
-            records = parsers.AmaxParser().parse(am_fp)
-            self.assertTrue(self.all_unique(am.water_year for am in records),
-                            msg="Records in {} do not have unique water years.".format(am_fp))
-            for i, am in enumerate(records):
-                self.assertGreater(am.water_year, 1800,
-                                   msg="Record {} in {} does not have a `water_year`>1800.".format(i + 1, am_fn))
-                self.assertLess(am.water_year, 2500,
-                                msg="Record {} in {} does not have a `water_year`<2500.".format(i + 1, am_fn))
-                self.assertGreater(am.flow, 0,
-                                   msg="Record {} in {} does not have a `flow`>0.".format(i + 1, am_fn))
-                self.assertLess(am.flow, 10000,
-                                msg="Record {} in {} does not have a `flow`<1000.".format(i + 1, am_fn))
+            yield self.check_am_file, am_fp
+
+    def check_am_file(self, am_fp):
+        am_fn = os.path.basename(am_fp)
+        records = parsers.AmaxParser().parse(am_fp)
+        nt.assert_true(self.all_unique(am.water_year for am in records),
+                       msg="Records in {} do not have unique water years.".format(am_fp))
+        for i, am in enumerate(records):
+            nt.assert_greater(am.water_year, 1800,
+                              msg="Record {} in {} does not have a `water_year`>1800.".format(i + 1, am_fn))
+            nt.assert_less(am.water_year, 2500,
+                           msg="Record {} in {} does not have a `water_year`<2500.".format(i + 1, am_fn))
+            nt.assert_greater(am.flow, 0,
+                              msg="Record {} in {} does not have a `flow`>0.".format(i + 1, am_fn))
+            nt.assert_less(am.flow, 10000,
+                           msg="Record {} in {} does not have a `flow`<1000.".format(i + 1, am_fn))
 
     def test_am_foreach_cd3(self):
         for cd3_fp in self._files_by_ext('.cd3'):
-            cd3_fn = os.path.basename(cd3_fp)
-            self.assertTrue(os.path.isfile(os.path.splitext(cd3_fp)[0] + '.am') or
-                            os.path.isfile(os.path.splitext(cd3_fp)[0] + '.AM'),
-                            msg="Catchment {} does not have a corresponding .am file.".format(cd3_fn))
+            yield self.check_am_for_cd3, cd3_fp
+
+    def check_am_for_cd3(self, cd3_fp):
+        cd3_fn = os.path.basename(cd3_fp)
+        nt.assert_true(os.path.isfile(os.path.splitext(cd3_fp)[0] + '.am') or
+                       os.path.isfile(os.path.splitext(cd3_fp)[0] + '.AM'),
+                       msg="Catchment {} does not have a corresponding .am file.".format(cd3_fn))
