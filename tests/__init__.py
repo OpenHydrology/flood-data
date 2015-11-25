65023629bfecda1f98960fb1b94efc11662870b7
Florenz A. P. Hollebrandse <faph@users.noreply.github.com>
2015-11-25 19:15:02 +0000

    tidy filepaths

--- tests/__init__.py
+++ tests/__init__.py
@@ -6,9 +6,12 @@ from floodestimation import parsers
 from floodestimation import entities
 
 
+DATA_FOLDER = './data'
+
+
 class TestValidFiles(object):
     def _files_by_ext(self, ext):
-        return [os.path.join(dp, f) for dp, dn, filenames in os.walk('./data')
+        return [f for dp, dn, filenames in os.walk(DATA_FOLDER)
                 for f in filenames if os.path.splitext(f)[1].lower() == ext]
 
     @staticmethod
@@ -17,12 +20,11 @@ class TestValidFiles(object):
         return not any(i in seen or seen.add(i) for i in x)
 
     def test_cd3_files(self):
-        for cd3_fp in self._files_by_ext('.cd3'):
-            yield self.check_cd3_file, cd3_fp
+        for cd3_fn in self._files_by_ext('.cd3'):
+            yield self.check_cd3_file, cd3_fn
 
-    def check_cd3_file(self, cd3_fp):
-        cd3_fn = os.path.basename(cd3_fp)
-        c = parsers.Cd3Parser().parse(cd3_fp)
+    def check_cd3_file(self, cd3_fn):
+        c = parsers.Cd3Parser().parse(os.path.join(DATA_FOLDER, cd3_fn))
         nt.assert_true(isinstance(c.descriptors.centroid_ngr, entities.Point),
                        msg="Catchment {} does not have centroid coordinates.".format(cd3_fn))
         nt.assert_greater(c.descriptors.dtm_area, 0,
@@ -39,14 +41,13 @@ class TestValidFiles(object):
                        msg="Catchment {} does not have a `saar`<5000.".format(cd3_fn))
 
     def test_am_files(self):
-        for am_fp in self._files_by_ext('.am'):
-            yield self.check_am_file, am_fp
+        for am_fn in self._files_by_ext('.am'):
+            yield self.check_am_file, am_fn
 
-    def check_am_file(self, am_fp):
-        am_fn = os.path.basename(am_fp)
-        records = parsers.AmaxParser().parse(am_fp)
+    def check_am_file(self, am_fn):
+        records = parsers.AmaxParser().parse(os.path.join(DATA_FOLDER, am_fn))
         nt.assert_true(self.all_unique(am.water_year for am in records),
-                       msg="Records in {} do not have unique water years.".format(am_fp))
+                       msg="Records in {} do not have unique water years.".format(am_fn))
         for i, am in enumerate(records):
             nt.assert_greater(am.water_year, 1800,
                               msg="Record {} in {} does not have a `water_year`>1800.".format(i + 1, am_fn))
@@ -58,11 +59,11 @@ class TestValidFiles(object):
                            msg="Record {} in {} does not have a `flow`<1000.".format(i + 1, am_fn))
 
     def test_am_foreach_cd3(self):
-        for cd3_fp in self._files_by_ext('.cd3'):
-            yield self.check_am_for_cd3, cd3_fp
+        for cd3_fn in self._files_by_ext('.cd3'):
+            yield self.check_am_for_cd3, cd3_fn
 
-    def check_am_for_cd3(self, cd3_fp):
-        cd3_fn = os.path.basename(cd3_fp)
+    def check_am_for_cd3(self, cd3_fn):
+        cd3_fp = os.path.join(DATA_FOLDER, cd3_fn)
         nt.assert_true(os.path.isfile(os.path.splitext(cd3_fp)[0] + '.am') or
                        os.path.isfile(os.path.splitext(cd3_fp)[0] + '.AM'),
                        msg="Catchment {} does not have a corresponding .am file.".format(cd3_fn))
